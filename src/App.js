01ab3964977d5eafdafc8bf235915abe3bdcae33
TabithaKoech <tabbyk1718@gmail.com>
2023-08-29 11:33:49 +0300

    Add existing project files to Git

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          Install WSL command
+          You can now install everything you need to run WSL with a single command. 
+          Open PowerShell or Windows Command Prompt in administrator mode by right-clicking and selecting 
+          "Run as administrator", enter the wsl --install command, then restart your machine.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
